920bb4af2f6f68a21a74761d22acf81c1420979b
Sergey Vinokurov <serg.foo@gmail.com>
2023-08-07 21:15:45 +0100

    Add my local utility scripts

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+#
+# File: clean.sh
+#
+# Created:  9 January 2022
+#
+
+# treat undefined variable substitutions as errors
+set -u
+# propagate errors from all parts of pipes
+set -o pipefail
+
+make extraclean
+# find . -name '*.elc' -delete
+# rm lisp/loaddefs.el
+
+exit 0
+
